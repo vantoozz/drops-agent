0f8541aac346bb488cd25fd5403a579dd2a03a24
Ivan Nikitin <vantoozz@gmail.com>
2019-01-04 14:41:08 +0300

    + listen udp

--- src/drops-agent.ts
+++ src/drops-agent.ts
@@ -1,8 +1,26 @@
 import {Message} from "./Message";
 import {StoreMessage} from "./Commands/StoreMessage";
 import {container} from "./bootstrap";
-// import dgram = require("dgram");
+import dgram = require("dgram");
 
-const message = new Message('lalala', new Date);
 const command = container.resolve<StoreMessage>(StoreMessage);
-command.handle(message);
+
+const server = dgram.createSocket('udp4');
+
+server.on('error', (err) => {
+    console.log(`server error:\n${err.stack}`);
+    server.close();
+});
+
+server.on('listening', () => {
+    const address = server.address();
+    console.log(address);
+});
+
+server.on('message', (msg) => {
+    (async (message: Message) => {
+        await command.handle(message)
+    })(new Message(msg.toString('utf8'), new Date));
+});
+
+server.bind(41234);
